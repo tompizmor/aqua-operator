14ba00c792bcb2d6cbecea44882b0b0ca988172d
nissim <nissim.bitan@aquasec.com>
2019-04-11 14:56:07 +0300

    Upload Aqua Operator

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ kubectl create -f  deploy/crds/operator_v1alpha1_aquaenforcer_crd.yaml
 kubectl create -f  deploy/crds/operator_v1alpha1_aquacsp_crd.yaml 
 kubectl create -f  deploy/crds/operator_v1alpha1_aquascanner_crd.yaml
 
-kubectl create secret docker-registry aqua-registry-secret --docker-server=registry.aquasec.com --docker-username=<AQUA_USERNAME> --docker-password=<AQUA_PASSWORD> --docker-email=<AQUA_EMAIL> -n aqua
-
 kubectl create -f deploy/service_account.yaml -n aqua
 kubectl create -f deploy/role.yaml
 kubectl create -f deploy/role_binding.yaml
